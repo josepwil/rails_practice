75c8894523f68799ede7f11f4ce378ba78a7a0cb
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-06 15:46:42 -0400

    routing root to pages controller home action, rendering home view

--- test_app_6/config/routes.rb
+++ test_app_6/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # roots to pages_controller hello method (no need to specify '_controller' it is expected by rails)
+  root 'pages#home'
 end
