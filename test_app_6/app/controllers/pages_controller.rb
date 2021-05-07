59d2ca2b822369f5875f7458b4e2ae5629d6e233
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-07 15:10:16 -0400

    create about page

--- test_app_6/app/controllers/pages_controller.rb
+++ test_app_6/app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  # empty method/action means rails expects a home.html.erb file in the views/pages folder
   def home
   end
+
+  def about
+  end
+  
   
 end
--- test_app_6/config/routes.rb
+++ test_app_6/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # roots to pages_controller hello method (no need to specify '_controller' it is expected by rails)
   root 'pages#home'
+  # a get request to the about page handled by the pages contoller about action/method
+  get 'about', to: 'pages#about'
 end
